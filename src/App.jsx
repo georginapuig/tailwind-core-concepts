5b67f8ac418756b23cae918b322a717059f216f6
georgina puig <georginaxpuig@gmail.com>
2023-06-26 19:17:10 -0300

    change layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,10 @@ import Fundamentals from './components/Fundamentals';
 function App() {
   return (
     <div>
-      <h1 className='text-xl'>Core Concepts</h1>
-
+      <h1 className='text-base font-semibold text-teal-300'>Core Concepts</h1>
+      <p className='text-3xl font-extrabold pt-2 pb-5'>
+        Utility-First Fundamentals
+      </p>
       <Fundamentals />
     </div>
   );
