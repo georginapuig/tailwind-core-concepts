9c5a42afc8fd9c0c461a77314e50cf34c68f12bb
georgina puig <georginaxpuig@gmail.com>
2023-07-27 19:16:43 -0300

    change layout

--- src/components/Fundamentals.jsx
+++ src/components/Fundamentals.jsx
@@ -2,7 +2,7 @@ import chatLogo from '.././img/logo.svg';
 
 export default function Fundamentals() {
   return (
-    <div className='bg-white max-w-sm mx-auto rounded-xl flex items-center p-6 shadow-lg space-x-4 text-left'>
+    <div className='mx-auto rounded-xl flex bg-white max-w-sm items-center p-6 shadow-lg space-x-4 text-left'>
       <div className='shrink-0'>
         <img className='h-12 w-12' src={chatLogo} alt='ChitChat Logo' />
       </div>
